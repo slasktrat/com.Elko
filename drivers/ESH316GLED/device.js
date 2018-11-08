1b0b673fe408bd338a8f539f4f9207e64912279b
Pål Raymond Johansen <35060010+prj84@users.noreply.github.com>
2018-11-08 15:00:42 +0100

    Cleaned up code

--- drivers/ESH316GLED/device.js
+++ drivers/ESH316GLED/device.js
@@ -1,75 +1,40 @@
-'use strict';
+"use strict";
 
 const Homey = require('homey');
+
 const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 
 const maxBrightness = 255;
 
-class ESH316GLED extends ZigBeeDevice {
+class ESH316GLED  extends ZigBeeDevice {
+
+	onMeshInit() {
+		this.enableDebug();
+		//this.printNode();
 
 
-  	// this method is called when the Device is inited
-  	onMeshInit() {
-      this.enableDebug();
+		//register onoff capability
+		if (this.hasCapability('onoff')) this.registerCapability('onoff', 'genOnOff');
+		//register att reportlisteners for onoff
+		this.registerAttrReportListener('genOnOff', 'onOff', 10, 3600, 1, value => {
+			this.log('onoff', value);
+			this.setCapabilityValue('onoff', value === 1);
+		}, 0);
 
+/*{return Promise.reject( new Error('Switching the device failed!') );}*/
 
-        		// register a capability listener
-        		this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
-            this.registerCapabilityListener('dim', this.onCapabilitydim.bind(this))
+		//register dim capability
+		if (this.hasCapability('dim')) this.registerCapability('dim', 'genLevelCtrl');
+		//register att reportlisteners for dim
+		this.registerAttrReportListener('genLevelCtrl', 'currentLevel', 10, 3600, 10, value => {
+			this.log('dim report', value);
+			this.setCapabilityValue('dim', value / maxBrightness);
+		}, 0);
 
-
-        		//register att reportlisteners for onoff
-        		this.registerAttrReportListener('genOnOff', 'onOff', 2, 300, 1, value => {
-        			this.log('onoff', value);
-        			this.setCapabilityValue('onoff', value === 1);
-        		}, 0);
-
-
-        		//register att reportlisteners for dim
-        		this.registerAttrReportListener('genLevelCtrl', 'currentLevel', 2, 300, 1, value => {
-        			this.log('dim report', value);
-        			this.setCapabilityValue('dim', value / maxBrightness);
-        		}, 0);
-
-
-        	}
-
-        	// this method is called when the Device has requested a state change (turned on or off)
-    async onCapabilityOnoff( value, opts, callback ) {
-
-        		// return promise, ignore
-        	try {
-            await this.node.endpoints[0].clusters['genOnOff'].do(value ? "on" : "off", {value}).catch(err => null);
-            console.log('onoff command sent');
-        	} catch(e) {
-            console.log('caught error', e);
-          }
-
-          // return resolved promise
-          return Promise.resolve ( );
-        }
-
-
-          // this method is called when the Device has requested a state change (dim)
-
-        // this method is called when the Device has requested a state change (turned on or off)
-    async onCapabilitydim( value, opts, callback ) {
-
-          // return promise, ignore
-          try {
-            await this.node.endpoints[0].clusters['genLevelCtrl'].do("moveToLevelWithOnOff", {level: (value * maxBrightness), transtime: 10}).catch(err => null);
-            console.log('dim command sent');
-          } catch(e) {
-            console.log('caught error', e);
-          }
-
-          // return resolved promise
-          return Promise.resolve( );
-        }
-
+		}
 }
- module.exports = ESH316GLED;
 
+module.exports = ESH316GLED;
 
 //─────────────── Logging stdout & stderr ───────────────
 //2018-08-11 06:58:25 [log] [ElkoApp] Elko App is running!
@@ -103,5 +68,4 @@ class ESH316GLED extends ZigBeeDevice {
 //2018-08-11 06:58:26 [log] [ManagerDrivers] [ESH316GLED] [0] ---- sid : attrs
 //2018-08-11 06:58:26 [log] [ManagerDrivers] [ESH316GLED] [0] ---- currentLevel : 254
 //2018-08-11 06:58:26 [log] [ManagerDrivers] [ESH316GLED] [0] ------------------------------------------
-//2018-08-11 06:58:26 [log] [ManagerDrivers] [ESH316GLED] [0] registerAttrReportListener() -> already configured attr reporting attrReport_0_genOnOff_onOff
-//2018-08-11 06:58:26 [log] [ManagerDrivers] [ESH316GLED] [0] registerAttrReportListener() -> already configured attr reporting attrReport_0_genLevelCtrl_currentLevel
+//Jon - 40764118 - jon.berntsen@elko.no
